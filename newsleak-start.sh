b7f8e425b89bf55aa81156a8dc34a502b716004d
MattesR <mattes.ruckdeschel@tuhh.de>
2020-10-06 14:01:25 +0200

    change sudo to su

--- newsleak-start.sh
+++ newsleak-start.sh
@@ -4,5 +4,5 @@ rm -f RUNNING_PID
 cp -n -r conf /etc/settings/
 cp -n -r data /etc/settings/
 chown newsleak:newsleak -R /etc/settings/
-sudo -u newsleak export NEWSLEAK_CONFIG=/etc/settings/conf/application.production.conf
-sudo -u newsleak bin/newsleak -Dconfig.file=/etc/settings/conf/application.production.conf
+su - newsleak -c "env NEWSLEAK_CONFIG=/etc/settings/conf/application.production.conf"
+su - newsleak -c "bin/newsleak -Dconfig.file=/etc/settings/conf/application.production.conf"
