ee8a43a5ea2920b088cd25c885d6d28a2c2f7e4b
MattesR <mattes.ruckdeschel@tuhh.de>
2020-10-09 11:54:44 +0200

    copy conf from template in entrypoint

--- newsleak-start.sh
+++ newsleak-start.sh
@@ -3,5 +3,6 @@ cd /opt/newsleak
 rm -f RUNNING_PID
 cp -n -r conf /etc/settings/
 cp -n -r data /etc/settings/
+cp /local/conf /etc/settings/conf/newsleak.properties
 chown newsleak:newsleak -R /etc/settings/
 gosu  newsleak /bin/sh -c 'bin/newsleak -Dconfig.file=/etc/settings/conf/application.production.conf'
